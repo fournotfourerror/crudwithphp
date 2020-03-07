16df213b38e1aca0dfe97ee91860cfc194416b46
fournotfourerror <fournotfourerror@gmail.com>
2020-03-07 10:52:49 +0530

    Updates

---
+++ precache-manifest.9594ef3eee4f074538a48f6f4ab08040.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00055b6e6ffc34a05167553a90a43242",
+    "url": "/crudwithphp/index.html"
+  },
+  {
+    "revision": "fc6d856ecf50ce398a98",
+    "url": "/crudwithphp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e2a149af373bf4fdef06",
+    "url": "/crudwithphp/static/js/2.619a2d84.chunk.js"
+  },
+  {
+    "revision": "fc6d856ecf50ce398a98",
+    "url": "/crudwithphp/static/js/main.8255160f.chunk.js"
+  },
+  {
+    "revision": "ecf80cf4fca2a968433a",
+    "url": "/crudwithphp/static/js/runtime-main.4b3ec80f.js"
+  }
+]);
